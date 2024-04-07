bd2d059a4633b9156b248af813a9e6b48b927ec1
lema samuel <lemasam10@gmail.com>
2024-04-07 18:14:41 +0300

    assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const removeMain = document.querySelector('main')
+removeMain.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+
+newHeader.textContent ="Lema is the champion"
